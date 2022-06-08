50237bfc794b760952fbd4b3658358512080dd35
junyan li <jiangzhonping@163.com>
2022-06-08 17:05:09 -0400

    fix no sign in page

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const msalConfig = {
     auth: {
       clientId: '80c1687b-440a-4dd6-811a-8c1d76be8129',
       authority: "https://login.microsoftonline.com/5c7d0b28-bdf8-410c-aa93-4df372b16203",
-      redirectUri: 'https://lotuswalking.github.io/'
+      redirectUri: 'https://lotuswalking.github.io/GraphJs'
     },
     cache: {
       cacheLocation: "localStorage",
